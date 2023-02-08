645bf7ae25a9a05ba3c8706617f9b3f31ab19589
WuXiaoYing1 <13531018377@163.com>
2023-02-08 20:02:51 +0800

    新增按钮和调整布局

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div class="main">
     <el-container >
       <el-space>
       <el-main >
@@ -10,46 +10,88 @@
           <div class="author-info-block" >
               <div class="author-png">
                 <a href="https://www.baidu.com" target="_blank"  class="avatar-link" >
-                  <el-avatar
+                  <el-avatar 
                   src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/default.640d9a7.png"
                     />
                 </a>
               </div>  
-              <div class="author-article">
                 <div class="author-name">
                   <a href="https://www.baidu.com" target="_blank"  class="avatar-link author-name" >
-                      <span>官方账号</span>
+                      <span style="max-width: 128px;">官方账号</span>
                       <span>
                         <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png" class="level">
                         <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/ffdbad884aa0e7884cbcf924226df6ce.svg">
                       </span>
                   </a>
-                </div>
                 <div class="time-readsum">
                     <span class="time">time</span>
                     <span class="readsum">   s</span>
                 </div>
               </div>
-              <!-- <div class="subscribe"> -->
-                <!-- <button   @click="favor"
+          </div>
+          <div class="author-article">
+                <span>123456</span>
+                </div>
+        </div>
+      </el-main>
+      <el-aside class="aside-author" >
+      <el-space direction="vertical">
+        <div class="author-info ">
+          <div class="author-info-block">
+            <div class="author-png"  >
+                  <a href="https://www.baidu.com" target="_blank"  class="avatar-link" >
+                    <el-avatar
+                    src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/default.640d9a7.png"
+                      />
+                  </a>
+                </div>
+            <div class="author-name">
+                <a href="https://www.baidu.com" target="_blank"  class="avatar-link author-name" >
+                    <span>官方账号</span>
+                    <span>
+                      <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png" class="level">
+                      <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/ffdbad884aa0e7884cbcf924226df6ce.svg">
+                    </span>
+                </a>
+                <div class="author-identity">
+                    <span>前端工程师</span>
+                </div>
+            </div>
+          </div>
+          <div class="button">
+              <div class="subscribe" >
+                <el-button   @click="favor"
                       class="my_button" :style="{backgroundColor:bg_color, color: ft_color,}"
                       @mouseenter="change()" @mouseleave="goback()">
                   {{content}}
-              </button> -->
-                <!-- <el-button>
-                    关注
-                </el-button> -->
-              <!-- </div> -->
-          </div>
+              </el-button>
+              </div>
+              <div class="message">
+                <a href="https://www.baidu.com" target="_blank"  class="avatar-link">
+                  <el-button class="my_button">
+                      私信
+                  </el-button>
+                </a>
+                  
+              </div>
+            </div>
+          <el-divider></el-divider>
+            <div>
+                <span>获得点赞数</span>
+            </div>
+            <div>
+              <span>文章被阅读</span>
+            </div>
+          
         </div>
-      </el-main>
-      <el-aside class="aside-author" width="300px" >
-      <el-space direction="vertical">
-        <div class="author-info ">
-            <span>作者姓名</span>
-        </div>
-        <div class="article-directory">
+        <div class="article-directory ">
             <span>目录</span>
+            <el-divider></el-divider>
+            <div class="directory">
+                <span>
+                  1234
+                </span>
+            </div>
         </div>
       </el-space>
         
@@ -59,35 +101,6 @@
   </div>
 </template>
 <style>
-  .el-main{
-    border: 1px solid hsla(0, 0, 59.2, 0.1);
-    border-radius: 2px;
-    min-width: 700px;
-    height: 100%;
-    max-width: 960px;
-    background-color: #f4f5f5;
-    margin-left: 100px;
-  }
-  .aside-author{
-    position: relative;
-    top: 0;
-    /* left: 50px; */
-    margin-bottom: 20px;
-    margin-left: 20px;
-  }
-  .author-info{
-    border-radius: 4px;
-    padding: 1.667rem;
-    background-color: #f4f5f5;
-    border: 1px solid hsla(0, 0, 59.2, 0.1);
-  }
-  .article-directory{
-    border-radius: 4px;
-    padding: 1.667rem;
-    background-color: #f4f5f5;
-    border: 1px solid hsla(0, 0, 59.2, 0.1);
-    
-  }
 </style>
 
 <script>
@@ -97,9 +110,9 @@ export default {
         data () {
             return{
                 liked:false,
-                content:'+关注',
-                bg_color:"#fef0f0",
-                ft_color:"#f56c6c",
+                content:'关注',
+                bg_color:"#1D7DFA",
+                ft_color:"#fff",
             }
         },
         methods:{
@@ -107,29 +120,29 @@ export default {
                 this.liked=!this.liked;
                 if(this.liked){
                     this.content="已关注";
-                    this.bg_color="#f56c6c";
-                    this.ft_color="#fef0f0";
+                    this.bg_color="#FFFFFF";
+                    this.ft_color="#000000";
                 }
 
                 else{
-                    this.content="+关注"
-                    this.bg_color="#fef0f0";
-                    this.ft_color="#f56c6c";
+                    this.content="关注"
+                    this.bg_color="#1D7DFA";
+                    this.ft_color="#fff";
 
                 }
             },
             change(){
-                this.bg_color="#ff9999";
-                this.ft_color="#fef0f0";
+                this.bg_color="#FFFFFF";
+                this.ft_color="#0000F0";
             },
             goback(){
                 if(this.liked){
-                    this.bg_color="#f56c6c";
-                    this.ft_color="#fef0f0";
+                    this.bg_color="#FFFFFF";
+                    this.ft_color="#00000";
                 }
                 else{
-                    this.bg_color="#fef0f0";
-                    this.ft_color="#f56c6c";
+                    this.bg_color="#1D7DFA";
+                    this.ft_color="#fff";
                 }
             }
         }
@@ -147,9 +160,3 @@ import {
 </script>
 
 
-<!-- <script>
-  import { ElButton } from 'element-plus'
-  export default {
-    components: { ElButton },
-  }
-</script> -->
