ad3e977a89828186001cfed465a6dc98670d485d
Wcilzo <911402070@qq.com>
2023-02-19 15:38:53 +0800

    merge test V1

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 仿写掘金官网
 本项目是一个基于 Nuxt.js 3 和 Element Plus 的 Vue 仿写掘金官网的项目。项目目的是展示如何使用 Nuxt.js 和 Element Plus 创建响应式的现代 Web 应用程序，同时展示如何使用这些工具来实现常见的 UI 组件和布局。
 
@@ -50,46 +49,3 @@ yarn dev
 ## 贡献者
 感谢以下贡献者对本项目的贡献：
 - Ikun 9527 小组
-=======
-.....
-
-.....
-
-
-# Nuxt 3 Minimal Starter
-
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
-
-## Setup
-
-Make sure to install the dependencies:
-
-```bash
-# yarn
-yarn install
-
-# npm
-npm install
-
-# pnpm
-pnpm install
-```
-
-## Development Server
-
-Start the development server on http://localhost:3000
-
-```bash
-npm run dev
-```
-
-✔️使用eslint和prettier</br>
-
-✔️已使用element-plus自动导入</br>
-
-✔️2月6日凌晨完成了主页文章部分的菜单UI</br>
-
-✔️计划2月6日完成文章部分的UI。</br>
-
-🕑完成主页跳转到文章工作,正在优化数据获取逻辑
->>>>>>> test
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-<template>
-    <div class="main">
-        <div class="allBox">
-            <div class="articleList">
-                <div class="header"></div>
-            </div>
-            <div class="rightBox">
-                <Advertise></Advertise>
-                <Author></Author>
-            </div>
-        </div>
-    </div>
-</template>
-
-<script lang='ts'>
-import { defineComponent } from 'vue'
-import Advertise from '../component/advertise.vue'
-import Author from '../component/author.vue'
-export default defineComponent({
-    setup() {
-        return {};
-    },
-    updated() {
-        window.scroll(0, 0);
-    },
-    components: { Advertise,Author }
-})
-</script>
-
-<style>
-* {
-    margin: 0;
-    padding: 0;
-}
-a{
-    text-decoration: none;
-}
-.main {
-    position: relative;
-    margin: 0 auto;
-    width: auto;
-    max-width: 960px;
-}
-
-.main .allBox {
-    margin-top: 5.20rem;
-}
-
-.main .allBox .articleList {
-    margin-right: 21.667rem;
-    width: 688px;
-    height: 5000px;
-    background-color: skyblue;
-}
-
-.rightBox {
-    position: absolute;
-    width: 15rem;
-    height: 600px;
-    top: 0;
-    right: 0;
-    z-index: 1;
-}
-
-</style>
-=======
 <template>
   <div class="contain">
     <HomeNav />
@@ -80,4 +13,3 @@ a{
   position: relative;
 }
 </style>
->>>>>>> test
